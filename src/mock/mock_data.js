51bbe1ce26194914a3bc03bf776f3cf2bc02baf2
E888888 <666677@163.com>
2026-01-29 03:16:06 +0800

    chore: 更新导航数据 - 2026/1/29 03:16:04

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -308,13 +308,6 @@ export const mockData = {
           "icon": "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHHFT949fUipzkiFOBH3fAiZZUCdYojwUyX2aTonS1aIwMrx6NUIsHfUHSLzjGJFxxmvXElItynqF0a5ZOcOPVkJjb0Su6YC_eziV7HwK6aK94oYkyaqO71Q4fbadV5pJbflgZJyjdQmm8qC9AHuvWFc-&format=source&h=307"
         },
         {
-          "id": "site-1763406576454",
-          "name": "搜片影视",
-          "url": "https://soupian.pro",
-          "description": "搜你想看的影片",
-          "icon": "https://f.start.me/soupian.pro"
-        },
-        {
           "id": "site-1763406690952",
           "name": "黑猫VIP视频",
           "url": "https://vip.heimaokeji.xyz",
@@ -322,11 +315,11 @@ export const mockData = {
           "icon": "https://www.faviconextractor.com/favicon/vip.heimaokeji.xyz"
         },
         {
-          "id": "site-1763406649180",
-          "name": "夜幕解析",
-          "url": "https://yemu.xyz/shouye",
-          "description": "全网VIP电影视频在线解析",
-          "icon": "https://f.start.me/yemu.xyz"
+          "id": "site-1763406576454",
+          "name": "搜片影视",
+          "url": "https://soupian.pro",
+          "description": "搜你想看的影片",
+          "icon": "https://f.start.me/soupian.pro"
         },
         {
           "id": "site-1763406807709",

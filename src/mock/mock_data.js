275fa0d8536bd6e7704a42cd415dadc49eafb4d0
E888888 <666677@163.com>
2026-04-15 13:17:25 +0800

    chore: 更新导航数据 - 2026/4/15 13:17:24

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -1,5 +1,21 @@
 export const mockData = {
-  "categories": [],
+  "categories": [
+    {
+      "id": "category-1776230206167",
+      "icon": "🔥",
+      "name": "热门推荐",
+      "order": 0,
+      "sites": [
+        {
+          "id": "site-1776230242114",
+          "name": "秘塔Ai搜索",
+          "url": "https://metaso.cn",
+          "description": "没有广告，直达结果",
+          "icon": "https://www.faviconextractor.com/favicon/metaso.cn"
+        }
+      ]
+    }
+  ],
   "title": "猫猫导航🐱",
   "search": "bing"
 }

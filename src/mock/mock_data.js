d09772206b3b4775e2f5affe6073586992e01462
E888888 <666677@163.com>
2026-04-15 13:40:17 +0800

    chore: 更新导航数据 - 2026/4/15 13:40:16

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -100,6 +100,70 @@ export const mockData = {
           "icon": "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHHFT949fUipzkiFOBH3fAiZZUCdYojwUyX2aTonS1aIwMrx6NUIsHfUHSLzjGJFxxtfv.xuM072.shtTcicejSmzwcAXQyyC1YW1hHr8XFF_t6V0SdPwAOJpZbKrdlthWo8rtfLndPWSt.q.xiBl8q0-&format=source&h=307"
         }
       ]
+    },
+    {
+      "id": "category-1776231097423",
+      "icon": "🏦",
+      "name": "银行门户",
+      "order": 3,
+      "sites": [
+        {
+          "id": "site-1776231149565",
+          "name": "中国银行",
+          "url": "https://www.boc.cn",
+          "description": "阅人生，爱相随",
+          "icon": "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHHFT949fUipzkiFOBH3fAiZZUCdYojwUyX2aTonS1aIwMrx6NUIsHfUHSLzjGJFxxou3rrixbsZrpASSlykbMaMbFNduI9NVDtMqLNBYDLdkMLtRw6wRP5bZ5MVO45MF.jvVCTeDvZ8jEOI.z1F4VRA-&format=webp&w=150"
+        },
+        {
+          "id": "site-1776231298452",
+          "name": "农业银行",
+          "url": "https://www.abchina.com",
+          "description": "财智有道，通达无限",
+          "icon": "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHHFT949fUipzkiFOBH3fAiZZUCdYojwUyX2aTonS1aIwMrx6NUIsHfUHSLzjGJFxxjSephw_Ru5ecBOc9FYH2pMUKZmDQUsViALKOZzY3DKv0SFGCtWvQxIPlw4JTzDnYk3pHzCTCGC4DBTrExCkZEc-&format=webp&w=150"
+        },
+        {
+          "id": "site-1776231188225",
+          "name": "工商银行",
+          "url": "https://www.icbc.com.cn",
+          "description": "你身边的银行，可信赖的银行",
+          "icon": "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHM0OYfiFeMI2p9MWie0CvL99U4GA1gf6_kayTt_kBblFwHwo8BW8JXlqfnYxKPmmBaJG7j2kE3yr7vNRNOXk2UNePAqljcOpKj5DCirvty.x7fpN3C.4vuvxhElwZ0b3pyo1NuTuzPPYEMYf8J_frPY-&format=webp&w=150"
+        },
+        {
+          "id": "site-1776231257014",
+          "name": "建设银行",
+          "url": "https://www.ccb.com",
+          "description": "建现在，见未来",
+          "icon": "https://play-lh.googleusercontent.com/b8G5ongWM5xpj5L19zISDrQ7z4vN-TlplHbURs1d50RVqiUvRsFe3rOsOAzBapW5Q5Mx=w480-h960-rw"
+        },
+        {
+          "id": "site-1776231448020",
+          "name": "邮政银行",
+          "url": "https://www.psbc.com",
+          "description": "邮银财富，伴您成就每一步",
+          "icon": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/90/26/91/90269117-3b66-12f1-f14e-25607ad5ee27/AppIcon-0-0-1x_U007emarketing-0-6-0-0-sRGB-85-220.png/400x400ia-75.webp"
+        },
+        {
+          "id": "site-1776231519929",
+          "name": "交通银行",
+          "url": "https://www.bankcomm.com",
+          "description": "诚信永恒，稳健致远",
+          "icon": "https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHM0OYfiFeMI2p9MWie0CvL99U4GA1gf6_kayTt_kBblFwHwo8BW8JXlqfnYxKPmmBUkIyT49rlmr_k0QQ2IK35c36_CcwrJGu7jZ3ogiY4hSwSvSE2KyBDnd7OA_Dq6awmHHG11sZQGZIB1ArVtsXKc-&format=webp&h=307"
+        },
+        {
+          "id": "site-1776231486111",
+          "name": "广发银行",
+          "url": "https://www.cgbchina.com.cn",
+          "description": "以心相交，成其久远",
+          "icon": "https://play-lh.googleusercontent.com/hh2QgdWbIsDzVMkJIr95zj6c568IuAC7s6iso_JH5z4GzilfLJCFz1sWiVUybxIQjc4t=w480-h960-rw"
+        },
+        {
+          "id": "site-1776231598694",
+          "name": "浦发银行",
+          "url": "https://www.spdb.com.cn",
+          "description": "给还未来的，一个未来",
+          "icon": "https://www.faviconextractor.com/favicon/www.spdb.com.cn"
+        }
+      ]
     }
   ],
   "title": "猫猫导航🐱",

ace0e4bb7847643840b530a492073f5e151d8f81
E888888 <666677@163.com>
2026-04-15 13:24:48 +0800

    chore: 更新导航数据 - 2026/4/15 13:24:47

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -12,11 +12,52 @@ export const mockData = {
           "url": "https://metaso.cn",
           "description": "没有广告，直达结果",
           "icon": "https://www.faviconextractor.com/favicon/metaso.cn"
+        },
+        {
+          "id": "site-1776230503437",
+          "name": "AmyMind",
+          "url": "https://amymind.com/zh-cn",
+          "description": "轻松创建思维导图",
+          "icon": "https://www.faviconextractor.com/favicon/amymind.com"
+        },
+        {
+          "id": "site-1776230543698",
+          "name": "微步云沙箱",
+          "url": "https://s.threatbook.com",
+          "description": "在线文件威胁分析",
+          "icon": "https://www.faviconextractor.com/favicon/s.threatbook.com"
+        },
+        {
+          "id": "site-1776230574483",
+          "name": "VirusTotal",
+          "url": "https://www.virustotal.com",
+          "description": "可疑文件、URL危险分析",
+          "icon": "https://f.start.me/www.virustotal.com"
+        },
+        {
+          "id": "site-1776230604774",
+          "name": "GitHub",
+          "url": "https://github.com",
+          "description": "代码托管平台",
+          "icon": "https://f.start.me/github.com"
+        },
+        {
+          "id": "site-1776230633269",
+          "name": "Wormhole",
+          "url": "https://wormhole.app",
+          "description": "简单，私密的文件分享",
+          "icon": "https://www.faviconextractor.com/favicon/wormhole.app"
+        },
+        {
+          "id": "site-1776230672878",
+          "name": "IP111",
+          "url": "https://www.ip111.cn",
+          "description": "全方位查询您的IP地址",
+          "icon": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsnlug2AL2iIc36Sx-oaz4Rro008wNwro_Zw&s"
         }
       ]
     }
   ],
-  "title": "East导航",
-  "search": "bing",
-  "_fileSha": "4e38a982c3a91c1b3c0bb4627515fdc07e602dd9"
+  "title": "猫猫导航🐱",
+  "search": "bing"
 }
